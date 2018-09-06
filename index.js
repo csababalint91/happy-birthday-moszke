f970cb021fa718cc472b88a0b935d9807f1b08b7
csababalint91 <40868013+csababalint91@users.noreply.github.com>
2018-09-06 18:09:02 +0100

    Upload website

---
+++ index.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("hidden");
+    if (x.style.display = "block") {
+        x.style.display === "none";
+    } else {
+        x.style.display = "none";
+    }
+}
+
